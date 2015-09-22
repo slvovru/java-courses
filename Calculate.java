60a764a0f018ef9922f842de23496cda5e507532
slvovru <i@slvov.ru>
2015-09-22 14:33:58 +0300

    New edition of Calculate

--- Calculate.java
+++ Calculate.java
@@ -6,7 +6,12 @@ public class Calculate {
 		int first = Integer.valueOf(args[0]);
 		int second = Integer.valueOf(args[1]);
 		int summ = first + second;
+		int razn = first - second;
+		int proizv = first * second;
+		double div = (second == 0 ? 0 : (double) first / (double) second);
 		System.out.println("sum  " + summ);
+		System.out.println("razn  " + razn);
+		System.out.println("proizv  " + proizv);
+		System.out.println("div  " + div);
 	}
-
 }
